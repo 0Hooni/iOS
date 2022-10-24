ed95cb814d0bb61c68ae6ec037c7ff2bbef84663
0Hooni <thddudgns972@gmail.com>
2022-10-25 00:52:42 +0900

    Create Starbucks FE clone
    Rising camp week2 Homework, Frontend cloning

---
+++ Rising Camp/Starbucks FE Clone/Starbucks FE Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Starbucks FE Clone
+//
+//  Created by 송영훈 on 2022/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

651acffd9c8949583c9a4a01542e5ad2264e5234
0Hooni <thddudgns972@gmail.com>
2022-11-09 20:45:30 +0900

    Update PJ
    + Recognize Jump button while game play
    + Change Cat image

--- Rising Camp/Cat Adventure/Cat Adventure/GameViewController.swift
+++ Rising Camp/Cat Adventure/Cat Adventure/GameViewController.swift
@@ -11,27 +11,23 @@ import AVFoundation
 class GameViewController: UIViewController {
     
     // MARK: Object & Var
-    @IBOutlet weak var Timer: UILabel!
+    @IBOutlet weak var timer: UILabel!
     var bgm = AVAudioPlayer()
     
     @IBOutlet weak var character: UIImageView!
-    @IBOutlet weak var attackButton: UIImageView!
-    @IBOutlet weak var jumpButton: UIImageView!
+    @IBOutlet weak var jumpButton: UIButton!
     
     
     // MARK: Function
-    @IBAction func tapJumpButton(_ sender: Any?) {
+    @IBAction func tapJumpButton(_ sender: UIButton) {
         print("DEBUG: Press Jump Button")
     }
-    @IBAction func tapAttackButton(_ sender: Any?) {
-        print("DEBUG: Press Attack Button")
-    }
     
+    // MARK: View Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         print("DEBUG: View Did Load")
-        
-        // MARK: AVAudio Setting
+        // AVAudio Setting
         guard let fileName = Bundle.main.url(forResource: "BGM", withExtension: "mp3")
         else { return }
         do {
@@ -44,26 +40,29 @@ class GameViewController: UIViewController {
         print("DEBUG: View will appear")
         
         // MARK: READY PART
-        print("DEBUG: Print READY")
-        self.Timer.text = "Ready"
-        sleep(1)
-        print("DEBUG: Print GET")
-        self.Timer.text = "Get"
-        sleep(1)
-        print("DEBUG: Print START")
-        self.Timer.text = "Start"
-        sleep(1)
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            print("DEBUG: Print READY")
+            self.timer.text = "Ready"
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            print("DEBUG: Print GET")
+            self.timer.text = "Get"
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            print("DEBUG: Print START")
+            self.timer.text = "Start"
+        }
         
         // MARK: Game Main Thread
-        DispatchQueue.global().async {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
             print("DEBUG: GAME START")
             print("DEBUG: Music Start")
             self.bgm.play()
             print("DEBUG: Timer start")
             // MARK: Timer thread
-            for i in (0...60).reversed() {
+            for i in (0...10).reversed() {
                 DispatchQueue.main.async {
-                    self.Timer.text = String(i)
+                    self.timer.text = String(i)
                     print("DEBUG: Tick Tok \(i)")
                 }
                 sleep(1)
@@ -74,5 +73,6 @@ class GameViewController: UIViewController {
         
     }
     
+    // MARK: @objc
 }
 

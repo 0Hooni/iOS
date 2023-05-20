a03b3a6deed04276f99f23176e24e1e90e047f4a
YeongHoon Song <thddudgns972@gmail.com>
2023-05-20 21:57:59 +0900

    Create Clock project
    For practice

---
+++ Practice/Clock/Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by 송영훈 on 2023/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

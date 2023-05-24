32390866ef9fb89ef92d6c683e6afea0f891645b
YeongHoon Song <thddudgns972@gmail.com>
2023-05-24 23:18:04 +0900

    Update project
    Current Time view - finish

--- Practice/Clock/Clock/Source/VC/NowTimeViewController.swift
+++ Practice/Clock/Clock/Source/VC/NowTimeViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 class NowTimeViewController: UIViewController {
     
     @IBOutlet weak var currentTime: UILabel!
+    var timerRepeat: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         currentTime.text = getCurrentTime()
+        startRepeatTimer()
     }
 
     func getCurrentTime() -> String{
+        
         let formatter = DateFormatter()
         formatter.dateFormat = "HH시 mm분 ss초"
         formatter.locale = Locale(identifier: "ko_KR")
         return formatter.string(from: Date())
+    }
+    func startRepeatTimer() {
+        let userInfo = "Timer 2"
+        timerRepeat = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFireRepeat(timer:)), userInfo: userInfo, repeats: true)
+    }
+    @objc func timerFireRepeat(timer: Timer) {
+        print("timerFireRepeat!")
+        currentTime.text = getCurrentTime()
+        if timer.userInfo != nil {
+            print("\(String(describing: timer.userInfo))")  // Timer 2
+            
+        }
     }
     
 }

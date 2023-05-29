5ca417a011af2b6b4f924b0e8910c04bc64be163
YeongHoon Song <thddudgns972@gmail.com>
2023-05-29 22:33:58 +0900

    Update project

--- Practice/Clone clock app/Clone clock app/Source/VC/AddCountryViewController.swift
+++ Practice/Clone clock app/Clone clock app/Source/VC/AddCountryViewController.swift
@@ -19,16 +19,6 @@ class AddCountryViewController: UIViewController {
         headerView.frame.size.height = headerTitleLabel.frame.height + headerSeachBar.frame.height
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+

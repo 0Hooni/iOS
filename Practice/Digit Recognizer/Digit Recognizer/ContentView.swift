faea2a2951bb4f7078106170dfb6cf4dd684bcb7
YeongHoon Song <thddudgns972@gmail.com>
2023-10-03 20:22:21 +0900

    Cleaning some repo

--- Practice/Digit Recognizer/Digit Recognizer/ContentView.swift
+++ Practice/Digit Recognizer/Digit Recognizer/ContentView.swift
@@ -2,43 +2,67 @@
 //  ContentView.swift
 //  Digit recognizer
 //
-//  Created by 송영훈 on 2023/09/25.
+//  Created by 송영훈 on 2023/09/27.
 //
 
 import SwiftUI
 import RealityKit
+import ARKit
+import CoreML
 
-struct ContentView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+class ARFrameRequest: ARView, ARSessionDelegate {
+    func setup() {
+        self.session.delegate = self
+        let config = ARWorldTrackingConfiguration()
+        self.session.run(config)
+    }
+    
+    func getPrediction(didUpdate frame: ARFrame) {
+        // 프레임 업데이트마다 호출됨
+        let framePixelBuffer = frame.capturedImage
+        
+        // MNIST model 을 model 변수에 할당
+        guard let model = try? MNISTClassifier(configuration: MLModelConfiguration())
+        else {
+            print("DEBUG: load model errer")
+            return
+        }
+        
+        // model의 prediction 함수를 이용하여 frame과 shape을 입력받음
+        guard let output = try? model.prediction(image: framePixelBuffer)
+        else {
+            print("DEBUG: Load Output Error")
+            return
+        }
+        
+        print(output.classLabel)
+        
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        getPrediction(didUpdate: frame)
     }
 }
 
-struct ARViewContainer: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> ARView {
+struct ARContainerView: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARFrameRequest {
+        let arView = ARFrameRequest(frame: .zero)
+        arView.setup()
         
-        let arView = ARView(frame: .zero)
-
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
+        
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARFrameRequest, context: Context) { }
     
 }
+
+struct ContentView: View {
+    var body: some View {
+        ARContainerView().edgesIgnoringSafeArea(.all)
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ Practice/Digit Recognizer/Digit Recognizer/Digit_recognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digit_recognizerApp.swift
+//  Digit recognizer
+//
+//  Created by 송영훈 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Digit_recognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
